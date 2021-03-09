faf41ead35d52a44eb3d3a784857f2ac335ccffe
Vadim-Dudar <haskervadim228@gmail.com>
2021-03-09 11:33:41 +0200

    add slug path(have a proble)

--- shop/urls.py
+++ shop/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
   path('shop', views.shop, name = 'shop' ),
   path('cart', views.cart_produckt, name = 'cart_produckt')
 ]
-for post in posts:
-  urlpatterns += path(f'{post.slug}', views.cart_produckt, name = f'{post.name}' )
+# for post in posts:
+#   urlpatterns += path(f'{post.slug}', views.cart_produckt, name = f'{post.name}' )
   
 if settings.DEBUG:
   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
